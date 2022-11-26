8acc371f1c39595ed67984b80dd36c85393b0e85
munozarturo <munoz.arturoroman@gmail.com>
2022-11-26 16:15:21 -0600

    changed main

--- __main__.py
+++
@@ -1,5 +0,0 @@
-from vval import validate, validate_iterable
-
-print("""
-
-""")
--- q.py
+++
@@ -1,5 +0,0 @@
-from typing import Callable, Union
-from vval import validate, validate_iterable
-
-x: list = [1, 2, 3]
-validate(x, int)
