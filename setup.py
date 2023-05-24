53231dd86e7a07e176284963e429b73b4484ba47
munozarturo <munoz.arturoroman@gmail.com>
2023-05-23 19:03:13 -0600

    package setup

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(
+    name="vval",
+    version="2.0.0",
+    author="Arturo Munoz",
+    author_email="munoz.arturoroman@gmail.com",
+    description="Input validation in Python",
+    long_description="""\
+        This package provides a simple way to validate user input in Python.
+    """,
+    long_description_content_type="text/markdown",
+    url="https://github.com/munozarturo/vval",
+    packages=["vval"],
+    classifiers=[
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.11",
+    install_requires=["varname==0.11.0", "typing_inspect==0.8.0"],
+)
