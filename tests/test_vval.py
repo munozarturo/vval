25176655d5c6e23ded972e8d6344fede0d5b21dd
Arturo Muñoz <101126374+munozarturo@users.noreply.github.com>
2023-11-14 12:46:39 -0500

    added is_generic tests

--- tests/test_vval.py
+++ tests/test_vval.py
@@ -1,4 +1,4 @@
-from typing import Union, List, Optional
+from typing import Union, List, Optional, Dict, Union
 from vval.vval import is_union, is_iterable, is_generic, is_callable
 
 
@@ -27,3 +27,12 @@ def test_is_iterable():
         pass
 
     assert not is_iterable(NonIterable())  # Non-iterable class instances
+
+
+def test_is_generic():
+    assert is_generic(List[int])  # Generic type
+    assert is_generic(Dict[str, int])  # Generic type
+    assert is_generic(Union[int, str])  # Union, a kind of generic
+    assert not is_generic(int)  # Not a generic type
+    assert not is_generic(3.14)  # Not a generic type
+    assert not is_generic("hello")  # Not a generic type
